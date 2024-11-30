d0a3f12fd39310905e8d1fd663db4ed72a92acae
Miguel Colque <micky.cm@outlook.es>
2024-11-30 12:58:29 -0400

    Tipos de datos y variables

---
+++ Basics/00_data-types.py
@@ -0,0 +1,19 @@
+# Esto es un comentario
+'''
+Comentario en varias lineas
+El uso de comillas simples o dobles es indistinto
+'''
+print('Hola Python') # Hola mundo en Python
+
+# Tipos de datos
+print(type(7)) # number
+print(type(2.14)) # float
+print(type('texto')) # string
+print(type(3 + 1j)) # complex number
+print(type(3 == 3)) # Bool (true)
+print(type(2 >= 3)) # Bool (false)
+print(type([1, 2, 3])) # List
+print(type((9.8, 3.14, 2.7))) # Tuple
+print(type({9.8, 3.14, 2.7})) # Set
+print(type({'name':'Asabeneh'})) # Dictionary
+
---
+++ Basics/variables.py
@@ -0,0 +1,34 @@
+# Variables
+my_str_variable = 'My string variable' # snake_case o minusculas
+print(my_str_variable)
+
+int_variable = 5
+print(int_variable)
+
+int_to_str_variable = str(int_variable)
+print(int_to_str_variable)
+print(type(int_to_str_variable))
+
+bool_variable = False
+print(bool_variable)
+
+# Funsion len()
+print(len(my_str_variable))
+
+# Concatenacion de variables en un print
+print(my_str_variable, int_to_str_variable, bool_variable)
+print('The value is: ', bool_variable)
+
+# Variables en una sola linea
+name, surname, nick, age = 'Miguel', 'Colque', 'Micky', 35
+
+# Concatenar con formato f""
+print(f"My name is: {name} {surname}, I'm {age} years old, and my nick is {nick}")
+
+# Input para pedir datos al usuario
+# Las variables pueden ser reasignadas
+name = input('What\'s your name? ') # Se usa \ para escapar caracteres reservados (\')
+age = input('How old are you? ')
+
+print(name)
+print(age)
